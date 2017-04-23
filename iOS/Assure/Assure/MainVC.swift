6519b7b4a6ea0b6b15fc32a45c8be8fd45990f4e
kennethshawfriedman <kennethshawfriedman@gmail.com>
2017-04-22 21:15:56 -0400

    added global look, beginnings of place to place grandpa status

--- iOS/Assure/Assure/MainVC.swift
+++ iOS/Assure/Assure/MainVC.swift
@@ -9,14 +9,33 @@
 import UIKit
 
 class MainVC: UIViewController {
+	
+	let grandpaStatusLabel = UILabel()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		
+		//Bottom View
 		let bottomView = UIView()
 		bottomView.backgroundColor = UIColor(colorLiteralRed: 237/255.0, green: 237/255.0, blue: 237/255.0, alpha: 1)
-		bottomView.frame = CGRect(x: 0, y: 555, width: view.frame.width, height: view.frame.width-555)
+		bottomView.frame = CGRect(x: 0, y: 555, width: view.frame.width, height: view.frame.height-555)
 		view.addSubview(bottomView)
+		
+		//Grandpa Status Bar
+		grandpaStatusLabel.backgroundColor = UIColor(colorLiteralRed: 201/255.0, green: 201/255.0, blue: 201/255.0, alpha: 1)
+		grandpaStatusLabel.frame = CGRect(x: 0, y: 529, width: view.frame.width, height: 26)
+		grandpaStatusLabel.text = "Grandpa is currently in the kitchen"
+		grandpaStatusLabel.textAlignment = NSTextAlignment.center
+		view.addSubview(grandpaStatusLabel)
+	}
+	
+	func setGrandpaLocationStatus() {
+		var gLocation = ""
+		
+		//LOGIC FOR NAMING LOCATION HERE
+		
+		grandpaStatusLabel.text = "Grandpa is currenty in the \(gLocation)"
 	}
 
 }
