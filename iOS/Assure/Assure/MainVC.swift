db4dbadfd1334721441dd51ee8effc24bd5b67a9
kennethshawfriedman <kennethshawfriedman@gmail.com>
2017-04-22 21:03:34 -0400

    MainVC work

---
+++ iOS/Assure/Assure/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainVC.swift
+//  Assure
+//
+//  Created by Kenneth Friedman on 4/22/17.
+//  Copyright © 2017 Avery Lamp. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+		
+		let bottomView = UIView()
+	}
+
+}
