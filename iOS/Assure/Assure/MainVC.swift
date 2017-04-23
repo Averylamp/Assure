0053ce8e7d3e5198f3f8c5d438b782c50553eb35
kennethshawfriedman <kennethshawfriedman@gmail.com>
2017-04-22 21:05:29 -0400

    bottomview

--- iOS/Assure/Assure/MainVC.swift
+++ iOS/Assure/Assure/MainVC.swift
@@ -13,9 +13,10 @@ class MainVC: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		
-		
 		let bottomView = UIView()
+		bottomView.backgroundColor = UIColor(colorLiteralRed: 237/255.0, green: 237/255.0, blue: 237/255.0, alpha: 1)
+		bottomView.frame = CGRect(x: 0, y: 555, width: view.frame.width, height: view.frame.width-555)
+		view.addSubview(bottomView)
 	}
 
 }
